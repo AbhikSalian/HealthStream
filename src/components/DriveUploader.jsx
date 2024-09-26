bd3bb9eee20affaf006c5b524fbdedcac8a9b9fa
Abhik Salian <21e03.abhik@sjec.ac.in>
2024-09-26 18:15:35 +0530

    removed unnecessary

--- src/components/DriveUploader.jsx
+++ src/components/DriveUploader.jsx
@@ -12,46 +12,6 @@ const DriveUploader = () => {
       return;
     }
 
-    // const token = await signInWithGoogle();
-    // console.log(token);
-    // setAccessToken(token);
-
-    // const blob = fetch(videoUrl).then((res) => res.blob());
-
-    // blob.then((videoBlob) => {
-    //   const file = new File([videoBlob], "recorded_video.webm", {
-    //     type: "video/webm",
-    //   });
-
-    //   const metadata = {
-    //     name: "recorded_video.webm",
-    //     mimeType: "video/webm",
-    //   };
-
-    //   const formData = new FormData();
-    //   formData.append(
-    //     "metadata",
-    //     new Blob([JSON.stringify(metadata)], { type: "application/json" })
-    //   );
-    //   formData.append("file", file);
-
-    //   fetch(
-    //     "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
-    //     {
-    //       method: "POST",
-    //       headers: new Headers({ Authorization: "Bearer " + token }),
-    //       body: formData,
-    //     }
-    //   )
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //       console.log("File uploaded successfully", data);
-    //     })
-    //     .catch((error) => {
-    //       console.error("Error uploading file:", error);
-    //     });
-    // });
-
     try {
       // const token = await signInWithGoogle();
       console.log(token);
